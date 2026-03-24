7430ad6fd007c8496300db31d2bc575fb0fc34b6
jEletu <jemimaheletu@gmail.com>
2026-03-24 13:01:38 +0000

    Add files via upload

---
+++ header.js
@@ -0,0 +1,30 @@
+export function renderHeader(user = null) {
+    const header = document.createElement('header');
+    header.className = 'main-header';
+    
+    header.innerHTML = `
+        <div class="logo">
+            <a href="/">MovieRecommender</a>
+        </div>
+        <nav class="nav-links">
+            <a href="/" class="nav-link">Home</a>
+            <a href="/trending" class="nav-link">Trending</a>
+            <a href="/recommendations" class="nav-link">For You</a>
+            ${user ? `
+                <div class="user-menu">
+                    <span class="username">${user.username}</span>
+                    <div class="dropdown">
+                        <a href="/profile">Profile</a>
+                        <a href="#" id="logout-btn">Logout</a>
+                    </div>
+                </div>
+            ` : `
+                <a href="/login" class="nav-link">Login</a>
+                <a href="/register" class="nav-link signup">Sign Up</a>
+            `}
+        </nav>
+    `;
+    
+    return header;
+}
+
