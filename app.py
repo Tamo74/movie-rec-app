a89b258317e1dd4f59b4fd2fa193dd899df5d1e4
tamo74 <tamiloreojo381@gmail.com>
2026-04-04 23:04:46 +0100

    commit static/js/script.js commit templates/base.html commit templates/dashboard.html commit templates/home.htnl commit user/model commit user/routes commit app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session 
+from functools import wraps
 import pymongo
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.secret_key=b'\xda\xfb\x01\xc3vc#\x9a\xc7\xb0\x7fk\xadUp\x06'
 # dabaseses
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.userLoginSystem 
 
+#decoraors
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        print("SESSION:", session)
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
+        
 
 #routes
 from user import routes
-
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/dashboard/')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
