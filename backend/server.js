8c3767d34442bffaaf134d12eca3740430020297
damitaaa <damita.stephanie@gmail.com>
2026-04-08 01:34:49 +0100

    update server.js for upcoming movies

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 
-const app= express();
+const app = express();
 app.use(cors());
 
 const PORT = 3000;
 
-const API_KEY = "ef0446eee648d392883577d34d4496e7"
+const API_KEY = "ef0446eee648d392883577d34d4496e7";
+
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
@@ -15,13 +16,35 @@ app.get("/", (req, res) => {
 app.get("/api/movies/trending", async (req, res) => {
     try {
         const response = await axios.get(
-            `https://api.themoviedb.org/3/trending/movie/day?api_key=${ef0446eee648d392883577d34d4496e7}`
+            `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
         );
-        res.json(response.dta.results);
+
+        res.json(response.data.results);
+
     } catch (error) {
         console.error(error);
             res.status(500).json({ error: "Failed to fetch movies" });
         }
+});
+
+app.get("/api/movies/upcoming", async (req, res) => {
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`
+        );
+
+        const today = new Date().toISOString().split("T")[0];
+
+        const filtered = response.data.results.filter(movie =>
+            movie.release_date > today
+        );
+
+        res.json(filtered);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to fetch upcoming movies" });
+    }
 });
 
 app.listen(PORT, () => {
